39d4fd561e70f2574afb3414e8966c380ec340ac
Theerawit Waithayawan <pm.theerawit@gmail.com>
2025-12-22 22:39:04 +0700

    feat: introduce Prisma ORM with initial database schema and client setup.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,12 +2,6 @@ import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-    datasources: {
-        db: {
-            url: "file:C:/Users/pmthe/OneDrive/Documents/Work/project/story-map_v3/StoryMap-V2/prisma/dev.db",
-        },
-    },
-})
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
